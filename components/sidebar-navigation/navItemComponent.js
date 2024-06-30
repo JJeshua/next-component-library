cfc6f0693f1b03706786a7cf55285e6c0e8efb25
Jeshua <79337598+JJeshua@users.noreply.github.com>
2024-06-30 12:18:21 -0700

    Nav item hover state

--- components/sidebar-navigation/navItemComponent.js
+++ components/sidebar-navigation/navItemComponent.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import styles from './sidebarNavigation.module.css';
+
 export const NavItemComponent = ({ title, icon, sidebarNavigationState }) => {
   return (
     <>
-      <div className="group flex items-center py-2 px-4 rounded-md hover:bg-blue-800 hover:text-slate-200 transition-colors duration-75">
+      <div className={`glass ${styles.nav_item} flex items-center py-2 px-4 rounded-md transition-colors duration-75`}>
         {icon && (
           <FontAwesomeIcon
-            className="group-hover:text-inherit text-neutral-300 pr-2 transition-colors duration-75"
+            className="size-4 text-neutral-300 pr-2 transition-colors duration-75"
             icon={icon}
           />
         )}
         {!sidebarNavigationState.sidebarNavigationCollapsed && (
           <a
             href="#"
-            className="group-hover:text-inherit text-neutral-300 transition-colors duration-75"
+            className="text-neutral-300 transition-colors duration-75"
           >
             {title}
           </a>
--- components/sidebar-navigation/sidebarNavigation.js
+++ components/sidebar-navigation/sidebarNavigation.js
@@ -7,7 +7,12 @@ import styles from './sidebarNavigation.module.css';
 
 // fontawesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faBell,
+  faGear,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { NavItemComponent } from './navItemComponent';
 
@@ -18,14 +23,33 @@ const SidebarNavigation = ({ ...rest }) => {
   const { sidebarNavigation } = useSidebarNavigation();
   return (
     <>
+      {/* Navbar wrapper and background */}
       <div
         className={`${styles.glass} relative top-0 left-0 h-full w-64 overflow-y-auto z-50`}
       >
+        {/* Content wrapper */}
         <div className="p-4">
           <nav className="space-y-2">
+            {/* Logo / header */}
+            <div></div>
             <NavItemComponent
               title="Home"
               icon={faHouse}
+              sidebarNavigationState={sidebarNavigation}
+            />
+            <NavItemComponent
+              title="Notifications"
+              icon={faBell}
+              sidebarNavigationState={sidebarNavigation}
+            />
+            <NavItemComponent
+              title="Settings"
+              icon={faGear}
+              sidebarNavigationState={sidebarNavigation}
+            />
+            <NavItemComponent
+              title="Profile"
+              icon={faUser}
               sidebarNavigationState={sidebarNavigation}
             />
           </nav>
