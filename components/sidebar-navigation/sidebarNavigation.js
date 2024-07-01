4ac0d2d8f0277fd114dc4a5decded7ff0923d59c
JJeshua <79337598+JJeshua@users.noreply.github.com>
2024-06-30 19:54:55 -0700

    Animated collapse

--- components/sidebar-navigation/sidebarNavigation.js
+++ components/sidebar-navigation/sidebarNavigation.js
@@ -31,81 +31,93 @@ const SidebarNavigation = ({ ...rest }) => {
   return (
     <>
       {/* Navbar wrapper and background */}
-      <div
-        className={`${styles.glass} relative top-0 left-0 h-full ${
-          !sidebarNavigation.sidebarNavigationCollapsed ? 'w-64' : 'w-fit'
-        } overflow-y-auto z-50`}
+      <CSSTransition
+        in={sidebarNavigation.sidebarNavigationCollapsed}
+        timeout={300}
+        classNames={{
+          enter: styles['shrink-enter'],
+          enterActive: styles['shrink-enter-active'],
+          enterDone: styles['shrink-enter-done'],
+          exit: styles['shrink-exit'],
+          exitActive: styles['shrink-exit-active'],
+          exitDone: styles['shrink-exit-done'],
+        }}
       >
-        {/* Content wrapper */}
-        <div className="p-4">
-          <nav className="space-y-2">
-            {/* Logo / header */}
-            <div className="flex items-center mb-8">
-              <FontAwesomeIcon
-                className={`size-12 text-blue-700 ${
-                  !sidebarNavigation.sidebarNavigationCollapsed ? 'pr-2' : ''
-                }`}
-                icon={faSquare}
-              />
-
-              <CSSTransition
-                in={!sidebarNavigation.sidebarNavigationCollapsed}
-                timeout={200}
-                classNames={{
-                  enter: styles['collapse-enter'],
-                  enterActive: styles['collapse-enter-active'],
-                  exit: styles['collapse-exit'],
-                  exitActive: styles['collapse-exit-active'],
-                }}
-                unmountOnExit
-              >
-                <div className="flex flex-col">
-                  <text className="text-neutral-300">BVT </text>
-                  <text className="text-sm text-neutral-500">Components</text>
-                </div>
-              </CSSTransition>
-
-              <CSSTransition
-                in={!sidebarNavigation.sidebarNavigationCollapsed}
-                timeout={200}
-                classNames={{
-                  enter: styles['collapse-enter'],
-                  enterActive: styles['collapse-enter-active'],
-                  exit: styles['collapse-exit'],
-                  exitActive: styles['collapse-exit-active'],
-                }}
-                unmountOnExit
-              >
+        <div
+          className={`${styles.glass} relative top-0 left-0 h-full w-64 overflow-y-auto z-50`}
+        >
+          {/* Content wrapper */}
+          <div className="p-4">
+            <nav className="space-y-2">
+              {/* Logo / header */}
+              <div className="flex items-center justify-center mb-8">
                 <FontAwesomeIcon
-                  className="cursor-pointer size-4 text-neutral-500 ml-auto"
-                  icon={faChevronLeft}
-                  onClick={() => _toggleCollapseSidebar()}
+                  className={`size-12 text-blue-700 ${
+                    !sidebarNavigation.sidebarNavigationCollapsed ? 'pr-2' : ''
+                  }`}
+                  icon={faSquare}
                 />
-              </CSSTransition>
-            </div>
-            <NavItemComponent
-              title="Home"
-              icon={faHouse}
-              sidebarNavigationState={sidebarNavigation}
-            />
-            <NavItemComponent
-              title="Notifications"
-              icon={faBell}
-              sidebarNavigationState={sidebarNavigation}
-            />
-            <NavItemComponent
-              title="Settings"
-              icon={faGear}
-              sidebarNavigationState={sidebarNavigation}
-            />
-            <NavItemComponent
-              title="Profile"
-              icon={faUser}
-              sidebarNavigationState={sidebarNavigation}
-            />
-          </nav>
+
+                <CSSTransition
+                  in={!sidebarNavigation.sidebarNavigationCollapsed}
+                  timeout={300}
+                  classNames={{
+                    enter: styles['collapse-enter'],
+                    enterActive: styles['collapse-enter-active'],
+                    exit: styles['collapse-exit'],
+                    exitActive: styles['collapse-exit-active'],
+                  }}
+                  unmountOnExit
+                >
+                  <div className="flex flex-col">
+                    <text className="text-neutral-300">BVT </text>
+                    <text className="text-sm text-neutral-500">Components</text>
+                  </div>
+                </CSSTransition>
+
+                <CSSTransition
+                  in={!sidebarNavigation.sidebarNavigationCollapsed}
+                  timeout={300}
+                  classNames={{
+                    enter: styles['collapse-enter'],
+                    enterActive: styles['collapse-enter-active'],
+                    exit: styles['collapse-exit'],
+                    exitActive: styles['collapse-exit-active'],
+                  }}
+                  unmountOnExit
+                >
+                  <FontAwesomeIcon
+                    className="cursor-pointer size-4 text-neutral-500 ml-auto"
+                    icon={faChevronLeft}
+                    onClick={() => _toggleCollapseSidebar()}
+                  />
+                </CSSTransition>
+              </div>
+              <NavItemComponent
+                title="Home"
+                icon={faHouse}
+                sidebarNavigationState={sidebarNavigation}
+              />
+              <NavItemComponent
+                title="Notifications"
+                icon={faBell}
+                sidebarNavigationState={sidebarNavigation}
+              />
+              <NavItemComponent
+                title="Settings"
+                icon={faGear}
+                sidebarNavigationState={sidebarNavigation}
+              />
+              <NavItemComponent
+                title="Profile"
+                icon={faUser}
+                sidebarNavigationState={sidebarNavigation}
+              />
+            </nav>
+          </div>
         </div>
-      </div>
+      </CSSTransition>
+      <button onClick={() => _toggleCollapseSidebar()}></button>
     </>
   );
 };
